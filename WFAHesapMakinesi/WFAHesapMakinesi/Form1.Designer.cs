f8e8c4c68fb282aa0b0b4ef7ca554af5366b3f49
Mehmet Mustafa Özcan <ozcan0615@gmail.com>
2023-02-03 23:31:31 +0300

    Hesap Makinesi çalışması

---
+++ WFAHesapMakinesi/WFAHesapMakinesi/Form1.Designer.cs
@@ -0,0 +1,370 @@
+﻿namespace WFAHesapMakinesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtncE = new System.Windows.Forms.Button();
+            this.btnC = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.btnCarp = new System.Windows.Forms.Button();
+            this.btnCikar = new System.Windows.Forms.Button();
+            this.btnTopla = new System.Windows.Forms.Button();
+            this.button19 = new System.Windows.Forms.Button();
+            this.btnBol = new System.Windows.Forms.Button();
+            this.btnKare = new System.Windows.Forms.Button();
+            this.btnKokal = new System.Windows.Forms.Button();
+            this.txtYazi = new System.Windows.Forms.TextBox();
+            this.lblSayilar = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtncE
+            // 
+            this.BtncE.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtncE.Location = new System.Drawing.Point(12, 120);
+            this.BtncE.Name = "BtncE";
+            this.BtncE.Size = new System.Drawing.Size(104, 35);
+            this.BtncE.TabIndex = 0;
+            this.BtncE.Text = "CE";
+            this.BtncE.UseVisualStyleBackColor = true;
+            this.BtncE.Click += new System.EventHandler(this.BtncE_Click);
+            // 
+            // btnC
+            // 
+            this.btnC.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnC.Location = new System.Drawing.Point(122, 120);
+            this.btnC.Name = "btnC";
+            this.btnC.Size = new System.Drawing.Size(104, 35);
+            this.btnC.TabIndex = 0;
+            this.btnC.Text = "C";
+            this.btnC.UseVisualStyleBackColor = true;
+            this.btnC.Click += new System.EventHandler(this.btnC_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Location = new System.Drawing.Point(12, 284);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(49, 35);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.Location = new System.Drawing.Point(67, 284);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(49, 35);
+            this.btn2.TabIndex = 0;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.Location = new System.Drawing.Point(122, 284);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(49, 35);
+            this.btn3.TabIndex = 0;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn6.Location = new System.Drawing.Point(122, 243);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(49, 35);
+            this.btn6.TabIndex = 0;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5.Location = new System.Drawing.Point(67, 243);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(49, 35);
+            this.btn5.TabIndex = 0;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.Location = new System.Drawing.Point(12, 243);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(49, 35);
+            this.btn4.TabIndex = 0;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn7.Location = new System.Drawing.Point(12, 202);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(49, 35);
+            this.btn7.TabIndex = 0;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn8.Location = new System.Drawing.Point(67, 202);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(49, 35);
+            this.btn8.TabIndex = 0;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn9.Location = new System.Drawing.Point(122, 202);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(49, 35);
+            this.btn9.TabIndex = 0;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btn0
+            // 
+            this.btn0.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn0.Location = new System.Drawing.Point(12, 325);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(78, 35);
+            this.btn0.TabIndex = 0;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // button15
+            // 
+            this.button15.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button15.Location = new System.Drawing.Point(96, 325);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(49, 35);
+            this.button15.TabIndex = 0;
+            this.button15.Text = ".";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // btnCarp
+            // 
+            this.btnCarp.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCarp.Location = new System.Drawing.Point(177, 202);
+            this.btnCarp.Name = "btnCarp";
+            this.btnCarp.Size = new System.Drawing.Size(49, 35);
+            this.btnCarp.TabIndex = 0;
+            this.btnCarp.Text = "X";
+            this.btnCarp.UseVisualStyleBackColor = true;
+            this.btnCarp.Click += new System.EventHandler(this.btnCarp_Click);
+            // 
+            // btnCikar
+            // 
+            this.btnCikar.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCikar.Location = new System.Drawing.Point(177, 243);
+            this.btnCikar.Name = "btnCikar";
+            this.btnCikar.Size = new System.Drawing.Size(49, 35);
+            this.btnCikar.TabIndex = 0;
+            this.btnCikar.Text = "-";
+            this.btnCikar.UseVisualStyleBackColor = true;
+            this.btnCikar.Click += new System.EventHandler(this.btnCikar_Click);
+            // 
+            // btnTopla
+            // 
+            this.btnTopla.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTopla.Location = new System.Drawing.Point(177, 284);
+            this.btnTopla.Name = "btnTopla";
+            this.btnTopla.Size = new System.Drawing.Size(49, 35);
+            this.btnTopla.TabIndex = 0;
+            this.btnTopla.Text = "+";
+            this.btnTopla.UseVisualStyleBackColor = true;
+            this.btnTopla.Click += new System.EventHandler(this.btnTopla_Click);
+            // 
+            // button19
+            // 
+            this.button19.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button19.Location = new System.Drawing.Point(151, 325);
+            this.button19.Name = "button19";
+            this.button19.Size = new System.Drawing.Size(75, 35);
+            this.button19.TabIndex = 0;
+            this.button19.Text = "=";
+            this.button19.UseVisualStyleBackColor = true;
+            this.button19.Click += new System.EventHandler(this.button19_Click);
+            // 
+            // btnBol
+            // 
+            this.btnBol.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBol.Location = new System.Drawing.Point(177, 161);
+            this.btnBol.Name = "btnBol";
+            this.btnBol.Size = new System.Drawing.Size(49, 35);
+            this.btnBol.TabIndex = 0;
+            this.btnBol.Text = "/";
+            this.btnBol.UseVisualStyleBackColor = true;
+            this.btnBol.Click += new System.EventHandler(this.btnBol_Click);
+            // 
+            // btnKare
+            // 
+            this.btnKare.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKare.Location = new System.Drawing.Point(99, 161);
+            this.btnKare.Name = "btnKare";
+            this.btnKare.Size = new System.Drawing.Size(72, 35);
+            this.btnKare.TabIndex = 0;
+            this.btnKare.Text = "x²";
+            this.btnKare.UseVisualStyleBackColor = true;
+            this.btnKare.Click += new System.EventHandler(this.btnKare_Click);
+            // 
+            // btnKokal
+            // 
+            this.btnKokal.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKokal.Location = new System.Drawing.Point(12, 161);
+            this.btnKokal.Name = "btnKokal";
+            this.btnKokal.Size = new System.Drawing.Size(81, 35);
+            this.btnKokal.TabIndex = 0;
+            this.btnKokal.Text = " √x";
+            this.btnKokal.UseVisualStyleBackColor = true;
+            this.btnKokal.Click += new System.EventHandler(this.btnKokal_Click);
+            // 
+            // txtYazi
+            // 
+            this.txtYazi.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtYazi.Location = new System.Drawing.Point(12, 84);
+            this.txtYazi.Name = "txtYazi";
+            this.txtYazi.Size = new System.Drawing.Size(214, 30);
+            this.txtYazi.TabIndex = 1;
+            // 
+            // lblSayilar
+            // 
+            this.lblSayilar.BackColor = System.Drawing.Color.DimGray;
+            this.lblSayilar.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblSayilar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSayilar.Location = new System.Drawing.Point(12, 40);
+            this.lblSayilar.Name = "lblSayilar";
+            this.lblSayilar.Size = new System.Drawing.Size(214, 30);
+            this.lblSayilar.TabIndex = 2;
+            this.lblSayilar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.label1.Location = new System.Drawing.Point(63, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Hesap Makinesi";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ClientSize = new System.Drawing.Size(239, 384);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblSayilar);
+            this.Controls.Add(this.txtYazi);
+            this.Controls.Add(this.btnC);
+            this.Controls.Add(this.btnTopla);
+            this.Controls.Add(this.btnCikar);
+            this.Controls.Add(this.btnKokal);
+            this.Controls.Add(this.btnKare);
+            this.Controls.Add(this.btnBol);
+            this.Controls.Add(this.btnCarp);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.button19);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.BtncE);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtncE;
+        private System.Windows.Forms.Button btnC;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button btnCarp;
+        private System.Windows.Forms.Button btnCikar;
+        private System.Windows.Forms.Button btnTopla;
+        private System.Windows.Forms.Button button19;
+        private System.Windows.Forms.Button btnBol;
+        private System.Windows.Forms.Button btnKare;
+        private System.Windows.Forms.Button btnKokal;
+        private System.Windows.Forms.TextBox txtYazi;
+        private System.Windows.Forms.Label lblSayilar;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WFAHesapMakinesi/WFAHesapMakinesi/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFAHesapMakinesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            txtYazi.Text += 0.ToString();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            txtYazi.Text += 1.ToString();
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            txtYazi.Text += 2.ToString();
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            txtYazi.Text += 3.ToString();
+
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            txtYazi.Text += 4.ToString();
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            txtYazi.Text += 5.ToString();
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            txtYazi.Text += 6.ToString();
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            txtYazi.Text += 7.ToString();
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            txtYazi.Text += 8.ToString();
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            txtYazi.Text += 9.ToString();
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+
+            if (txtYazi.Text != "" && !txtYazi.Text.Contains('.'))
+            {
+                txtYazi.Text += ",";
+            }
+        }
+
+        private void btnTopla_Click(object sender, EventArgs e)
+        {
+            lblSayilar.Text += txtYazi.Text + " + ";
+            txtYazi.Clear();
+        }
+
+        private void btnC_Click(object sender, EventArgs e)
+        {
+            txtYazi.Text = string.Empty;
+        }
+
+
+
+        private void btnBol_Click(object sender, EventArgs e)
+        {
+            lblSayilar.Text += txtYazi.Text + " / ";
+            txtYazi.Clear();
+        }
+
+        private void btnCarp_Click(object sender, EventArgs e)
+        {
+            lblSayilar.Text += txtYazi.Text + " * ";
+            txtYazi.Clear();
+        }
+
+        private void btnCikar_Click(object sender, EventArgs e)
+        {
+            lblSayilar.Text += txtYazi.Text + " -";
+            txtYazi.Clear();
+        }
+        private void button19_Click(object sender, EventArgs e)
+        {
+            double sonuc = 0;
+            int i = 0;
+            lblSayilar.Text += txtYazi.Text;
+            string sayilarım = lblSayilar.Text;
+
+            
+            DataTable calc = new DataTable();          
+
+
+            txtYazi.Text=calc.Compute(lblSayilar.Text, " ").ToString();
+            
+        }
+
+        private void BtncE_Click(object sender, EventArgs e)
+        {
+            lblSayilar.Text = string.Empty;
+            txtYazi.Text = string.Empty;
+        }
+
+        private void btnKokal_Click(object sender, EventArgs e)
+        {
+
+           lblSayilar.Text=( Math.Sqrt(Convert.ToDouble(txtYazi.Text))).ToString();
+            txtYazi.Text = string.Empty;
+        }
+
+        private void btnKare_Click(object sender, EventArgs e)
+        {
+            lblSayilar.Text = (Math.Pow(Convert.ToDouble(txtYazi.Text),2)).ToString();
+            txtYazi.Text = string.Empty;
+        }
+
+        
+    }
+}
