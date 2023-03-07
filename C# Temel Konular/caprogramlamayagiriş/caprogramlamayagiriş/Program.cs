247b2d091abe5e95846b8cc16608ececb09af4f9
Mehmet Mustafa Özcan <ozcan0615@gmail.com>
2023-03-07 23:13:27 +0300

    C# Giris dersleri

---
+++ C# Temel Konular/caprogramlamayagiriş/caprogramlamayagiriş/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caprogramlamayagiriş
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Syntax (sözdizimi) Kuralları
+             * 1. c# programlama dilinde büyük küçük haf duyarlılığı vardır.
+             * 2.c# programlama dilinde türkçe karakter kullanılamaz.
+             * 3.c# programlarında kullandığınız komut satırları sonuna noktalı virgül ifadesi kullanılır. Bu ifade satırın bittiğini ifade eder.
+             * 4.c# programalama dilinde metinsel ifadeler '' arasında yazılmakatadır. Bu ifadeler içerisinde Türkçe karakter kullanabilirsiniz.
+             */
+            Console.WriteLine("Hello world");
+            Console.ReadLine();
+
+        }
+    }
+}
